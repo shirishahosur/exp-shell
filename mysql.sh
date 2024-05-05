be938b20ec03b400a518b8f710f5b72222e72ac7
shirisha hosur <shirisha.h916@gmail.com>
2024-05-05 10:55:09 +0530

    ansible

--- mysql.sh
+++ mysql.sh
@@ -37,5 +37,16 @@ VALIDATE $? "Enabling MySQL Server"
 systemctl start mysqld &>>$LOGFILE
 VALIDATE $? "Starting MySQL Server"
 
-mysql_secure_installation --set-root-pass ExpenseApp@1  &>>$LOGFILE
-VALIDATE $? "setting up root password"
+#mysql_secure_installation --set-root-pass ExpenseApp@1  &>>$LOGFILE
+#VALIDATE $? "setting up root password"
+
+#below code will be useful for idempotent nature
+
+mysql -h db.vedanshika.online -uroot -pExpenseApp@1 -e 'show databases;' &>>$LOGFILE
+if [ $? -ne 0 ]
+then
+    mysql_secure_installation --set-root-pass ExpenseApp@1  &>>$LOGFILE
+    VALIDATE $? "MySQL Root password Setup"
+else
+    echo -e "MySQL Root password is already setup...$Y SKIPPING $N"
+fi
